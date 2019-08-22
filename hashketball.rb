118d9115a8edeb418d249423a3039574947e3b68
Brett Saxon <brettcsaxon@gmail.com>
2019-08-22 14:49:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,42 @@
 # Write your code here!
 
+def game_hash
+game_hash = {:home =>
+  {:team_name => "Brooklyn Nets",
+  :colors => ["black , white"],
+  :players => [
+      {"Brook Lopez" => { :number => "11",
+                          :shoe => "17",
+                          :points => "17",
+                          :rebounds => "19",
+                          :assists => "10",
+                          :steals => "3",
+                          :blocks => "1",
+                          :slam_dunks => "15"}},
+       {"Jason Terry" =>  { :number => "31",
+                          :shoe => "15",
+                          :points => "19",
+                          :rebounds => "2",
+                          :assists => "2",
+                          :steals => "4",
+                          :blocks => "11",
+                          :slam_dunks => "1"}},
+      {"Mason Plumlee" => {:number => "1",
+                          :shoe => "19",
+                          :points => "26",
+                          :rebounds => "11",
+                          :assists => "6",
+                          :steals => "3",
+                          :blocks => "8",
+                          :slam_dunks => "5"}},
+  ]
+}
 
+:away =>
+:team_name => "Charlotte Hornets",
+:colors => "",
+:players => [],
 
+}
 
-
-
-
-
-
-
+end
